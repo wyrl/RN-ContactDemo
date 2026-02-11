452004fb5ff79407528edfc13057cbd5f7bb2637
Wyr'l Villazorda <wyrlvillazorda@gmail.com>
2026-02-12 03:30:08 +0800

    update

--- backend.js
+++ backend.js
@@ -1,7 +1,21 @@
 const express = require("express");
-const app = express();
+const os = require("os");
 
+const app = express();
 app.use(express.json());
+
+// function to get local WiFi IP
+function getLocalIP() {
+  const interfaces = os.networkInterfaces();
+  for (const name of Object.keys(interfaces)) {
+    for (const iface of interfaces[name]) {
+      if (iface.family === "IPv4" && !iface.internal) {
+        return iface.address;
+      }
+    }
+  }
+  return "0.0.0.0";
+}
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -9,12 +23,15 @@ app.get("/", (req, res) => {
 
 app.post("/api/contacts", (req, res) => {
   const { contacts } = req.body;
-
   console.log("Contacts count:", contacts.length);
-
   res.json({ status: "ok" });
 });
 
-app.listen(3000, "0.0.0.0", () => {
-  console.log("Server running on 3000 (public in WiFi)");
+const PORT = 3000;
+const HOST = "0.0.0.0"; // public in WiFi
+
+app.listen(PORT, HOST, () => {
+  //const ip = getLocalIP();
+  console.log("Server running:");
+  console.log(`👉 ${PORT}`);
 });
